acc75814d91b6c730226b2e3eabd234c465c17ab
ryansang55 <76501192+ryansang55@users.noreply.github.com>
2021-01-11 10:46:05 +0900

    0111_1045

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(function(){
     $('#top_popup .close').click(function(){
         $('#top_popup').fadeOut()
-    })
-})
+    }) /*#top_popup .close end*/
+
+
+        var sta = 1;
+        $('.top_icon .all_nav').click(function(){
+            if (sta == 1){
+                $(this).addClass('on');
+                sta = 2;
+                }else {
+                    $(this).removeClass('on');
+                sta = 1;
+                }
+        })/*.top_icon .all_nav*/
+
+}) /*jquery end */
